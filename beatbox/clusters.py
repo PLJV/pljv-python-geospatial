3c27d62a01ca3f3409c456678654e97abbf362ed
Kyle Taylor <kyle.a.taylor@gmail.com>
2019-02-20 12:44:36 -0700

    house keeping

--- beatbox/clusters.py
+++ beatbox/clusters.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
-def min_max_normalize(v=None):
-    return v 
+def min_max_center(df=None): return df.apply(lambda x: ( x - x.min() / x.max() - x.min() ) )
+def mean_variance_center(df=None): return df.apply(lambda x: ( x - x.mean() / x.std() ) )
